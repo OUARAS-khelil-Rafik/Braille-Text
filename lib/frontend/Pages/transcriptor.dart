336a4cc7c53e668a83e7edcfb323ecb627a9a093
Logan OUARAS <Logan-Ouaras@6p5dk4.onmicrosoft.com>
2023-04-16 05:33:38 +0100

    This is new Version

--- lib/frontend/Pages/transcriptor.dart
+++ lib/frontend/Pages/transcriptor.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: file_names, non_constant_identifier_names, use_build_context_synchronously
+// ignore_for_file: file_names, non_constant_identifier_names, use_build_context_synchronously, avoid_print
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import '../Bars/NavBar.dart';
